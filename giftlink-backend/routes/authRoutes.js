be6f93eea053ed1a26f7409f577323741e327ba2
rose-kg <rosekgatle99@gmail.com>
2025-09-06 03:07:56 +0200

    Implemented API endpoints

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,3 +1,38 @@
+router.post('/login', async (req, res) => {
+  try {
+    // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+    const db = await connectToDatabase();
+    // Task 2: Access MongoDB `users` collection
+    const collection = db.collection("users");
+    // Task 3: Check for user credentials in database
+    const theUser = await collection.findOne({ email: req.body.email });
+    if (theUser) {
+      // Task 4: Check if the password matches the encrypted password
+      let result = await bcryptjs.compare(req.body.password, theUser.password);
+      if (!result) {
+        logger.error('Passwords do not match');
+        return res.status(404).json({ error: 'Wrong password' });
+      }
+      // Task 5: Fetch user details from database
+      const userName = theUser.firstName;
+      const userEmail = theUser.email;
+      // Task 6: Create JWT authentication if passwords match with user._id as payload
+      let payload = {
+        user: {
+          id: theUser._id.toString(),
+        },
+      };
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+      res.json({ authtoken, userName, userEmail });
+    } else {
+      // Task 7: Send appropriate message if user not found
+      logger.error('User not found');
+      return res.status(404).json({ error: 'User not found' });
+    }
+  } catch (e) {
+    return res.status(500).send('Internal server error');
+  }
+});
 // Step 1 - Task 2: Import necessary packages
 const express = require('express');
 const app = express();
